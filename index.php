b00f356098c062ac407e41298aac47fa4965b17c
Syd <sydneyx421@gmail.com>
2020-03-10 21:05:54 +0100

    fin de configuration de l'architechture avec installation de composer et twig et test d'utilisation de twig

--- index.php
+++ index.php
@@ -12,12 +12,23 @@ use App\Library\Route;
 require 'Library\Autoloader.php';
 require 'Library\functions.php';
 Autoloader::register();
+require 'vendor/autoload.php';
 $router = new RouterPOO();
 
+//chemin vers nos view
+$templatePath = realpath(__DIR__.'/View');
+
+$loader = new \Twig\Loader\FilesystemLoader($templatePath);
+$twig = new \Twig\Environment($loader, ['cache'=>false]);
+
+echo $twig->render('test.twig', ['coucou'=>'helloWorld']);
+die; // juste pour l'exemple
 // ------------------ Route(URL, nomDuController, nomDeLaction)
 
 //$router->addRoute(new Route('/home', 'blog', 'home'));
 
 //pages réserver à l'admin
+
+
 
 $router->run();
